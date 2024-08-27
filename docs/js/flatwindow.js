31f7ab7b4c33b70da661b64fe19933630ec08e0a
FlyGues2889 <youxiang@qq.com>
2024-08-27 11:00:53 +0800

    20240827

--- docs/js/flatwindow.js
+++
@@ -1,8 +0,0 @@
-function showPopup() {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
-}
-function hidePopup() {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "none";
-}
